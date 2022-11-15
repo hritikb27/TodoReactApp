1d6ebb0b0135aa60fcb2ab4109fd9b9e42823e2f
unknown <hritikb90@gmail.com>
2022-11-15 18:12:30 +0530

    added font, icons  etc. and fixed login button logic

--- src/components/login.js
+++ src/components/login.js
@@ -1,13 +1,15 @@
 import { useEffect, useRef, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { validateEmail, classNames, saveState, loadState } from '../utils'
+import userIcon from '../assets/userIcon.png'
+import lockIcon from '../assets/lockIcon.png'
 
 const Login = () => {
     const [email, setEmail] = useState(undefined)
     const [password, setPassword] = useState(undefined)
     const [emailValidation, setEmailValidation] = useState(true)
     const [passwordValidation, setPasswordValidation] = useState(true)
-    const loginDisableRef = useRef(true);
+    const [loginDisable, setLoginDisable] = useState(true);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -29,14 +31,14 @@ const Login = () => {
         setEmail(email)
     }
 
-    const handlePassword = (password) => {
-        if (password.length >= 4 && password.length <= 16) {
+    const handlePassword = (passwordInput) => {
+        if (passwordInput.length >= 4 && passwordInput.length <= 16) {
             setPasswordValidation(true)
         }
         else {
             setPasswordValidation(false)
         }
-        setPassword(password)
+        setPassword(passwordInput)
     }
 
     const handleLogin = async () => {
@@ -54,34 +56,45 @@ const Login = () => {
         // })
 
         // const result = await loginCall.json() */
-        
+
         navigate('/todo')
     }
 
     useEffect(() => {
         // make login button disabled when input fields are empty and until all validations are not passed
-        if(!email && !password) loginDisableRef.current = true
-        if((emailValidation && passwordValidation) && (email && password)){
-            loginDisableRef.current = false
+        if(!email && !password) setLoginDisable(true)
+        if(!emailValidation || !passwordValidation) setLoginDisable(true)
+        else if((emailValidation && passwordValidation) && (email && password)){
+            setLoginDisable(false)
         }
     }, [emailValidation, passwordValidation])
 
     return (
         <div className="flex items-center h-screen w-full">
             <div className="w-full bg-white rounded shadow-lg p-8 m-4 md:max-w-sm md:mx-auto">
-                <span className="block w-full text-xl text-center uppercase font-bold mb-4">Rapptr Labs</span>
+                <span className="block w-full text-2xl text-center uppercase font-bold mb-4">Rapptr Labs</span>
                 <form className="mb-4" action="/" method="post">
-                    <div className="mb-4 md:w-full">
+                    <div className="relative mb-4 md:w-full">
                         <label htmlFor="email" className="block text-xs mb-1">Email</label>
-                        <input className={classNames(emailValidation ? '' : 'border-red-400', "w-full border-2 rounded p-2 outline-none focus:shadow-outline")} value={email} onChange={(e) => handleEmail(e.target.value)} type="email" name="email" placeholder="user@rapptrlabs.com" />
+                        <div className="relative text-gray-600 focus-within:text-gray-400">
+                            <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+                                <img src={userIcon} className='top-7 w-[25px]' alt="user-email" />
+                            </span>
+                            <input className={classNames(emailValidation ? '' : 'border-red-400', "w-full h-[40px] py-2 text-sm text-white border border-black bg-white rounded pl-10 focus:outline-none focus:bg-white focus:text-gray-900")} value={email} onChange={(e) => handleEmail(e.target.value)} type="email" name="email" placeholder="user@rapptrlabs.com" />
+                        </div>
                         {!emailValidation && <span className="text-red-500 ">Not a valid email</span>}
                     </div>
                     <div className="mb-6 md:w-full">
                         <label htmlFor="password" className="block text-xs mb-1">Password</label>
-                        <input className={classNames(passwordValidation ? '' : 'border-red-400', "w-full border-2 rounded p-2 outline-none focus:shadow-outline")} value={password} onChange={(e) => handlePassword(e.target.value)} type="password" name="password" placeholder="Must be at least 4 characters" />
+                        <div className="relative text-gray-600 focus-within:text-gray-400">
+                            <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+                                <img src={lockIcon} className='top-7 w-[25px]' alt="user-password" />
+                            </span>
+                            <input className={classNames(passwordValidation ? '' : 'border-red-400', "w-full h-[40px] py-2 text-sm text-white border border-black bg-white rounded pl-10 focus:outline-none focus:bg-white focus:text-gray-900")} value={password} onChange={(e) => handlePassword(e.target.value)} type="password" name="password" placeholder="Must be at least 4 characters" />
+                        </div>
                         {!passwordValidation && <span className="text-red-500 ">Not a valid password</span>}
                     </div>
-                    <button disabled={loginDisableRef.current} onClick={handleLogin} className={classNames((email && password) && (emailValidation && passwordValidation) ? '' : 'opacity-50', "w-full border-t border-l-2 border-r-4 border-b-4 border-black shadow-2xl bg-blue-500 hover:bg-blue-700 text-white uppercase text-sm font-semibold px-4 py-2 rounded")}>Login</button>
+                    <button disabled={loginDisable} onClick={handleLogin} className={classNames((email && password) && (emailValidation && passwordValidation) ? '' : 'opacity-50', "w-full border-t border-l-2 border-r-4 border-b-4 border-black shadow-2xl bg-blue-500 hover:bg-blue-700 text-white uppercase text-sm font-semibold px-4 py-2 rounded")}>Login</button>
                 </form>
             </div>
         </div>
