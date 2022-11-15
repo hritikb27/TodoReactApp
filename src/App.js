959a4568c649f5a084f0adeb27be7f772cab9fd7
unknown <hritikb90@gmail.com>
2022-11-15 20:54:47 +0530

    added basename to router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import Login from "./components/login";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TodoList from "./components/Todos/todolist";
 
 function App() {
 
   return (
     <div className="w-full flex justify-center">
-      <HashRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/todo" element={<TodoList />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
