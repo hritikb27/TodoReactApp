eeb69c86e10c0e4bdfa95d6a13edceb525b61d27
unknown <hritikb90@gmail.com>
2022-11-15 17:15:43 +0530

    fixed login button

--- src/components/login.js
+++ src/components/login.js
@@ -40,20 +40,22 @@ const Login = () => {
     }
 
     const handleLogin = async () => {
-        saveState('login', {email, password})
-        // response is not coming back successfully due to cors policy issue, maybe cors is not set with allow all origins to the server
-        const loginCall = await fetch('http://dev.rapptrlabs.com/Tests/scripts/user-login.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            mode: 'cors',
-            body: JSON.stringify({ email: 'test@rapptrlabs.com', password: 'Test123' })
-        })
-
-        const result = await loginCall.json()
         // save login data in localstorage
         saveState('login', {email, password})
+        // response is not coming back successfully due to cors policy issue, maybe cors is not set with allow all origins to the server
+
+        /* const loginCall = await fetch('http://dev.rapptrlabs.com/Tests/scripts/user-login.php', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     mode: 'cors',
+        //     body: JSON.stringify({ email: 'test@rapptrlabs.com', password: 'Test123' })
+        // })
+
+        // const result = await loginCall.json() */
+        
+        navigate('/todo')
     }
 
     useEffect(() => {
@@ -71,13 +73,15 @@ const Login = () => {
                 <form className="mb-4" action="/" method="post">
                     <div className="mb-4 md:w-full">
                         <label htmlFor="email" className="block text-xs mb-1">Email</label>
-                        <input className={classNames(emailValidation ? '' : 'border-red-400', "w-full border rounded p-2 outline-none focus:shadow-outline")} value={email} onChange={(e) => handleEmail(e.target.value)} type="email" name="email" id="email" placeholder="user@rapptrlabs.com" />
+                        <input className={classNames(emailValidation ? '' : 'border-red-400', "w-full border-2 rounded p-2 outline-none focus:shadow-outline")} value={email} onChange={(e) => handleEmail(e.target.value)} type="email" name="email" placeholder="user@rapptrlabs.com" />
+                        {!emailValidation && <span className="text-red-500 ">Not a valid email</span>}
                     </div>
                     <div className="mb-6 md:w-full">
                         <label htmlFor="password" className="block text-xs mb-1">Password</label>
-                        <input className={classNames(passwordValidation ? '' : 'border-red-400', "w-full border rounded p-2 outline-none focus:shadow-outline")} value={password} onChange={(e) => handlePassword(e.target.value)} type="password" name="password" id="password" placeholder="Must be at least 4 characters" />
+                        <input className={classNames(passwordValidation ? '' : 'border-red-400', "w-full border-2 rounded p-2 outline-none focus:shadow-outline")} value={password} onChange={(e) => handlePassword(e.target.value)} type="password" name="password" placeholder="Must be at least 4 characters" />
+                        {!passwordValidation && <span className="text-red-500 ">Not a valid password</span>}
                     </div>
-                    <Link to='/todo'><button disabled={loginDisableRef.current} onClick={handleLogin} className={classNames((email && password) && (emailValidation && passwordValidation) ? '' : 'opacity-50', "w-full border-t border-l-2 border-r-4 border-b-4 border-black shadow-2xl bg-blue-500 hover:bg-blue-700 text-white uppercase text-sm font-semibold px-4 py-2 rounded")}>Login</button></Link>
+                    <button disabled={loginDisableRef.current} onClick={handleLogin} className={classNames((email && password) && (emailValidation && passwordValidation) ? '' : 'opacity-50', "w-full border-t border-l-2 border-r-4 border-b-4 border-black shadow-2xl bg-blue-500 hover:bg-blue-700 text-white uppercase text-sm font-semibold px-4 py-2 rounded")}>Login</button>
                 </form>
             </div>
         </div>
